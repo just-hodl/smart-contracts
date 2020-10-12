7ab137accd7173410d67eba139247ecd11372e51
Space Eth <meta.ether21@gmail.com>
2020-10-12 14:46:27 +0300

    Changed marketing/future rewards funds rts.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,8 @@ const moment = require('moment');
 const JustHodl = artifacts.require('JustHodl.sol');
 const JHRewardsTimeLock = artifacts.require('JHRewardsTimeLock.sol');
 const stakingRewardsReleaseTS = moment().add(15, 'days').unix();
-const futureRewardsReleaseTS = moment().add(30, 'days').unix();
-const marketingFundsReleaseTS = moment().add(45, 'days').unix();
+const futureRewardsReleaseTS = moment().add(45, 'days').unix();
+const marketingFundsReleaseTS = moment().add(30, 'days').unix();
 
 module.exports = async function(deployer) {
   await deployer.deploy(JustHodl, { gas: process.env.GAS_VALUE });
