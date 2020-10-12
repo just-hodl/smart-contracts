4c3258a21e0fece1714fd764932286f876930863
Space Eth <meta.ether21@gmail.com>
2020-10-12 13:08:42 +0300

    Updated error messages.

--- contracts/JustHodlBase.sol
+++ contracts/JustHodlBase.sol
@@ -63,7 +63,7 @@ contract JustHodlBase is Context, IERC20 {
 
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
         _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "JustHodlBase: transfer amount exceeds allowance"));
         return true;
     }
 
@@ -73,33 +73,33 @@ contract JustHodlBase is Context, IERC20 {
     }
 
     function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "JustHodlBase: decreased allowance below zero"));
         return true;
     }
 
     function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(sender != address(0), "JustHodlBase: transfer from the zero address");
+        require(recipient != address(0), "JustHodlBase: transfer to the zero address");
 
         _beforeTokenTransfer(sender, recipient, amount);
 
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[sender] = _balances[sender].sub(amount, "JustHodlBase: transfer amount exceeds balance");
         _balances[recipient] = _balances[recipient].add(amount);
         emit Transfer(sender, recipient, amount);
     }
 
     function _softTransfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(sender != address(0), "JustHodlBase: transfer from the zero address");
+        require(recipient != address(0), "JustHodlBase: transfer to the zero address");
 
         _beforeTokenTransfer(sender, recipient, amount);
 
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[sender] = _balances[sender].sub(amount, "JustHodlBase: transfer amount exceeds balance");
         _balances[recipient] = _balances[recipient].add(amount);
     }
 
     function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
+        require(account != address(0), "JustHodlBase: mint to the zero address");
 
         _beforeTokenTransfer(address(0), account, amount);
 
@@ -109,33 +109,33 @@ contract JustHodlBase is Context, IERC20 {
     }
 
     function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: burn from the zero address");
+        require(account != address(0), "JustHodlBase: burn from the zero address");
 
         _beforeTokenTransfer(account, address(0), amount);
 
-        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+        _balances[account] = _balances[account].sub(amount, "JustHodlBase: burn amount exceeds balance");
         _totalSupply = _totalSupply.sub(amount);
         emit Transfer(account, address(0), amount);
     }
 
     function _penalty(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: penalty from the zero address");
+        require(account != address(0), "JustHodlBase: penalty from the zero address");
 
         _beforeTokenTransfer(account, address(0), amount);
 
-        _balances[account] = _balances[account].sub(amount, "ERC20: penalty amount exceeds balance");
+        _balances[account] = _balances[account].sub(amount, "JustHodlBase: penalty amount exceeds balance");
     }
 
     function _penaltyFrom(address account, uint256 amount) internal virtual {
-        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "ERC20: penalty amount exceeds allowance");
+        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "JustHodlBase: penalty amount exceeds allowance");
 
         _approve(account, _msgSender(), decreasedAllowance);
         _penalty(account, amount);
     }
 
     function _approve(address owner, address spender, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+        require(owner != address(0), "JustHodlBase: approve from the zero address");
+        require(spender != address(0), "JustHodlBase: approve to the zero address");
 
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
